32b56ff0fc10b5f944d0d3101a0f5e86deecfd2e
Sai <saidcooldude@gmail.com>
2026-03-15 15:53:11 -0700

    fixing path to be the right org

--- scripts/coordinator.py
+++ scripts/coordinator.py
@@ -41,7 +41,7 @@ import requests
 # Configuration
 # ---------------------------------------------------------------------------
 
-HUB_ORG = "autoresearch-at-home"
+HUB_ORG = "austin_mac"
 API_URL = "https://api.ensue-network.ai/"
 KEY_FILE = ".autoresearch-key"
 NAMESPACE = "autoresearch-go-ane-infer"
